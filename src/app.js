058828c154b2d1963f5d014296f317c88e5a026f
ozgisin <88027806+ozgisin@users.noreply.github.com>
2021-12-22 18:36:20 +0300

    Add mongo connection

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config');
+
+mongoose.connect(config.mongo.uri,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+
+
+
+
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+const app = express()
+
+
+app.get('/', function (req, res) {
+  res.send('Hello Resor')
+})
+
+app.listen(config.httPort, ()=> { 
+  console.log("Server is running at port 3000"); 
+});
+ 
---
+++ src/config.js
@@ -0,0 +1,8 @@
+const config = {
+    httPort: 3000,
+    mongo: {
+        uri: 'mongodb://localhost:27017/resor',
+    },
+};
+
+module.exports = config;
