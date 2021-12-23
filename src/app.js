0c722432eab999745aff134744230d6abd6c55e3
ozgisin <88027806+ozgisin@users.noreply.github.com>
2021-12-23 23:05:32 +0300

    Add linter

--- src/app.js
+++ src/app.js
@@ -2,33 +2,23 @@ const express = require('express');
 const mongoose = require('mongoose');
 const config = require('./config');
 
-mongoose.connect(config.mongo.uri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
-);
-
-
-
-
-
+mongoose.connect(config.mongo.uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', () => {
+  console.log('Connected successfully');
 });
 
+const app = express();
 
-const app = express()
-
-
-app.get('/', function (req, res) {
-  res.send('Hello Resor')
-})
-
-app.listen(config.httPort, ()=> { 
-  console.log("Server is running at port 3000"); 
+app.get('/', (request, res) => {
+  res.send('Hello Resor');
 });
- 
+
+app.listen(config.httPort, () => {
+  console.log('Server is running at port 3000');
+});
--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    httPort: 3000,
-    mongo: {
-        uri: 'mongodb://localhost:27017/resor',
-    },
+  httPort: 3000,
+  mongo: {
+    uri: 'mongodb://localhost:27017/resor',
+  },
 };
 
 module.exports = config;
